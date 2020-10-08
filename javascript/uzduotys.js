2caadbd1e7b528244329828e2176501e2b2e7c9d
Manvydas Difartas <manvydas.difartas@gmail.com>
2020-10-08 21:26:47 +0300

    js kd 1

---
+++ javascript/uzduotys.js
@@ -0,0 +1,18 @@
+let a = 1;
+console.log("Kintamojo a reikšmė: " + a);
+a = 2;
+console.log("Kintamojo a reikšmė: " + a);
+
+let b = 'Manvydas Difartas';
+let c = 'Mano vardas';
+console.log(c + ' ' + b);
+
+let gim = new Date(1990, 03, 29);
+let year = 1990;
+let month = 03;
+let date = 29;
+let sum = (gim.getFullYear() % 10) + (gim.getMonth() % 10) + (gim.getDate()% 10);
+console.log("Jūsų gimimo data yra: " + year + "-" + month + "-" + date);
+console.log("Paskutinių skaitmenų sudarytų skaičių suma yra: " + sum);
+
+let d,e,f,g;
