5280719801b2631188311ad35cd9f80f3156c3df
Manvydas Difartas <manvydas.difartas@gmail.com>
2020-10-12 21:37:23 +0300

    js 3

---
+++ uzduotys/ciklai.js
@@ -0,0 +1,20 @@
+let ats_elem = document.getElementById('ats');
+let ats_elem2 = document.getElementById('ats2');
+let ats_elem3 = document.getElementById('ats3');
+
+let i = k = 0;
+
+
+while (i < 10) {
+    ats_elem.innerHTML += '<br>' + i;
+    i++;
+}
+
+for (let j = 0; j < 10; j++) {
+    ats_elem2.innerHTML += '<br>' + j;
+}
+
+do {
+    ats_elem3.innerHTML += '<br>' + k;
+    k++;
+} while (k < 10)
