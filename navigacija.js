77f96de0d05c957c89e38578b56fb4c8c71eab40
Manvydas Difartas <manvydas.difartas@gmail.com>
2020-10-14 20:47:06 +0300

    navigacija

--- navigacija.js
+++ navigacija.js
@@ -1,11 +1,15 @@
 let turiniai = document.querySelectorAll('main > section');
+let meniuElementai = document.querySelectorAll('header nav > a');
 
-function pakeistiPuslapi (pslId) {
+function pakeistiPuslapi (meniuElem, pslId) {
     for (var i = 0; turiniai.length > i; i++) {
         turiniai[i].style.display = 'none';
+        meniuElementai[i].classList.remove('active');
 
     }
+    meniuElem.classList.add('active');
     document.getElementById(pslId).style.display = 'block';
 }
 
-pakeistiPuslapi('contacts');
+let defaultElem = document.querySelector('nav a:first-child');
+pakeistiPuslapi(defaultElem, 'about');
